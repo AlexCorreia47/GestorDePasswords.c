089833f7423bf8d2e3373f0a5b76b2b924af2caf
Coutilha <44584005+Coutilha@users.noreply.github.com>
2018-11-22 10:10:18 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,32 +6,66 @@ int Sair();
 int MenuEdit();
 int MenuUtilizador();
 int MenuVisualizar();
+int MenuUsername();
+
 int main()
 {
-    printf("\n ****************************MENU PRINCIPAL****************************");
-    MenuUtilizador();
-    MenuEdit();
-    MenuVisualizar();
-    Sair();
+    int opcaoMenuPrincipal;
 
+    printf("****************************MENU PRINCIPAL****************************");
+    printf("\n\n (1) Inserir novos dados de utilizador");
+    printf("\n (2) Editar dados introduzidos");
+    printf("\n (3) Visualizar dados introduzidos");
+    printf("\n (0) Sair");
+    scanf("%d", &opcaoMenuPrincipal);
 
+    switch(opcaoMenuPrincipal)
+    case 1:
+case 2:
+case 3:
+case 0:
     return 0;
 }
 
-  int MenuUtilizador()
-  {
-      printf("\n\n (1) Inserir novos dados de utilzador");
+int MenuUtilizador()
+{
+    int opcaoMenuUtilizador;
 
-  }
-  int MenuEdit()
-  {
-      printf("\n\n (2) Editar dados introduzidos");
-  }
-  int MenuVisualizar()
-  {
-      printf("\n\n (3) Visualizar dados introduzidos");
-  }
-  int Sair()
-  {
-      printf("\n\n (0) Sair");
-  }
+    printf("****************************MENU UTILIZADOR****************************");
+    printf("\n\n (1) Inserir Username");
+    printf("\n (2) Voltar ao menu principal");
+    scanf("%d", &opcaoMenuUtilizador);
+
+    return opcaoMenuUtilizador;
+}
+int MenuEdit()
+{
+    int opcaoMenuEdit;
+
+    printf("****************************MENU EDITAR****************************");
+    printf("\n\n (1) Inserir Username");
+    printf("\n (2) Voltar ao menu principal");
+    scanf("%d", &opcaoMenuEdit);
+
+    return opcaoMenuEdit;
+}
+int MenuVisualizar()
+{
+    int opcaoMenuVisualizar;
+
+    printf("****************************MENU VISUALIZAR****************************");
+    printf("\n\n (1) Inserir Username");
+    printf("\n (2) Voltar ao menu principal");
+    scanf("%d", &opcaoMenuVisualizar);
+
+    return opcaoMenuVisualizar;
+}
+int Sair()
+{
+
+}
+
+int MenuUsername()
+{
+
+}
